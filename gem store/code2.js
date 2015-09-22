7735cf23659ee8dd74a8652b3855d4fe852b25f7
Shwethads24 <dss.music24@gmailcom>
2015-09-22 16:09:44 +0530

    Sass framework

---
+++ gem store/code2.js
@@ -0,0 +1,22 @@
+
+  var app = angular.module('gemStore', []);
+
+  app.controller('StoreController', function(){
+    this.product = gems;
+  });
+
+  var gems = [{
+      name: 'Azurite',
+      description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
+      shine: 8,
+      price: 110.50,
+      rarity: 7,
+      color: '#CCC',
+      faces: 14,
+      images: [
+        "images/gem-02.gif",
+        "images/gem-05.gif",
+        "images/gem-09.gif"
+      ]
+      }];
+
